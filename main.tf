4231cbcd35d4c06855ed1ac6442ead25006948d9
Aman Dhalla <amandhalla33@gmail.com>
2026-04-10 21:05:14 +0530

    add app deployment via ssm

--- main.tf
+++ main.tf
@@ -123,9 +123,13 @@ resource "aws_launch_template" "lt" {
               apt install -y nginx
               systemctl start nginx
               systemctl enable nginx
-              echo "<h1>Server $(hostname)</h1>" > /var/www/html/index.html
               EOF
   )
+
+iam_instance_profile {
+  arn = aws_iam_instance_profile.ec2_profile.arn
+}
+
 }
 
 ##################################
@@ -192,4 +196,32 @@ resource "aws_autoscaling_group" "asg" {
   }
 
   target_group_arns = [aws_lb_target_group.tg.arn]
+}
+
+# IAM Role for EC2 (SSM Access)
+resource "aws_iam_role" "ec2_role" {
+  name = "ec2-ssm-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+}
+
+# Attach SSM Policy
+resource "aws_iam_role_policy_attachment" "ssm_attach" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+# Instance Profile
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "ec2-ssm-profile"
+  role = aws_iam_role.ec2_role.name
 }
